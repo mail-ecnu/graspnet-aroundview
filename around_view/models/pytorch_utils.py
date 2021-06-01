b721a7ea449e8f2e7b9a6f6901896a919d88c2b0
陈伟文 <cww970329@qq.com>
2021-06-01 23:23:53 +0800

    txd nb, prepare

--- around_view/models/pytorch_utils.py
+++ around_view/models/pytorch_utils.py
@@ -5,6 +5,8 @@ from torch.autograd import Variable
 def to_var(x):
     if type(x).__module__ == 'numpy':
         x = torch.from_numpy(x)
+    elif isinstance(x, (int, list)):
+        x = torch.tensor(x)
     if torch.cuda.is_available():
         x = x.cuda()
     return x
--- around_view/models/rnn.py
+++ around_view/models/rnn.py
@@ -9,7 +9,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__fil
 ROOT_DIR = os.path.dirname(BASE_DIR)
 sys.path.append(ROOT_DIR)
 from models.backbone import Pointnet2Backbone
-from around_view.models import pytorch_utils as ptu
+from around_view.models.pytorch_utils import to_var
 from around_view.utils.view_find import ViewSelector
 from around_view.utils.dataset import VIEW_LEN
 
@@ -17,6 +17,7 @@ from around_view.utils.dataset import VIEW_LEN
 class PreMLP(nn.Module):
     def __init__(self, points_num=256, feat_dim=1024, out_dim=512):
         super().__init__()
+        self.w0 = Pointnet2Backbone()
         self.w1 = nn.Linear(points_num, 1)
         self.w2 = nn.Sequential(
             nn.ReLU(),
@@ -24,33 +25,36 @@ class PreMLP(nn.Module):
             nn.ReLU(),
         )
 
-    def forward(self, x):               # b, point, dim
+    def forward(self, pointcloud):               # b, point, dim
         ''' x: seed_features of `b * [256, 1024]`
             out: features of `b * [out_dim]`
         '''
-        x = x.permute(0, 2, 1)          # b, dim_in, point
-        x = self.w1(x).squeeze()        # b, dim_in
-        return self.w2(x)               # b, dim_out
+        point_feature, _, _ = self.w0(pointcloud)
+        feature_point = point_feature.permute(0, 2, 1)          # b, dim_in, point
+        feature = self.w1(feature_point).squeeze()        # b, dim_in
+        return self.w2(feature)               # b, dim_out
 
 
 class RNNController(nn.Module, ViewSelector):
-    def __init__(self, cfgs, device, input_feature_dim=0):
+    def __init__(self, cfgs, device):
         super().__init__()
         ViewSelector.__init__(self, cfgs)
 
         self.device = device
-        self.emb_size = 512
+        # for views
+        self.views_emb_size = 128
+        # for points
+        self.point_emb_size = 512
         self.hidden_size = 512
 
         # self.answer_seq_len = answer_seq_len
         # self.weight_size = weight_size
-        
-        # self.emb = nn.Embedding(input_size, emb_size)  # embed inputs
 
-        self.backbone = Pointnet2Backbone(input_feature_dim)
-        self.preMLP = PreMLP(256, 1024, self.emb_size)        
-        self.enc = nn.LSTM(self.emb_size, self.hidden_size, batch_first=True)
-        self.dec = nn.LSTMCell(self.emb_size, self.hidden_size)  # LSTMCell's input is always batch first
+        self.enc = nn.Embedding(VIEW_LEN, self.views_emb_size)  # embed inputs(views)
+
+        self.backbone = PreMLP(256, 1024, self.point_emb_size)
+        # self.enc = nn.LSTM(self.emb_size, self.hidden_size, batch_first=True)
+        self.dec = nn.LSTMCell(self.views_emb_size, self.hidden_size)  # LSTMCell's input is always batch first
 
         self.hiedden = (torch.randn(1, 1, 3),
                         torch.randn(1, 1, 3))
@@ -64,17 +68,26 @@ class RNNController(nn.Module, ViewSelector):
         return idx
 
     def forward(self, batch_data):
-        self.first_view()
-        batch_selected_mask = ptu.to_var(np.repeat(self.selected_mask[np.newaxis, :], batch_data.shape[0], axis=0))
+        bs = batch_data.shape[0]
+        
+        
+        
+        view_candidate = self.enc(to_var(torch.arange(VIEW_LEN).repeat(bs, 1)))
+        selected_views = to_var(torch.empty((bs, 0)))
 
-        example_one_view_data = batch_data[:, 0, :, :]
-        point_feats, _, _ = self.backbone(example_one_view_data)
-        cloud_feats = self.preMLP(point_feats)
+        v = to_var(self.first_view()).repeat(bs, 1)                 # [bs, 1]
+        for i in range(self.max_view):
+            selected_views = torch.cat((selected_views, v), 1)      # [bs, i+1]
+            view_data = batch_data[torch.arange(bs), v.view(-1)]
+            cloud_feats = self.backbone(view_data)
+
+            import ipdb; ipdb.set_trace()
+            print('rua')
+            
 
         import ipdb; ipdb.set_trace()
         x_0 = 0
         h_0 = 0
         c_0 = 0
 
-        import ipdb; ipdb.set_trace()
         return seed_features
